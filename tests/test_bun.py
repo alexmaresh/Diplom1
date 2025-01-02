f239e95ec70c18ef8e8a27af114eb2e6ada9f88c
Alexandra Maresh <maresh.al@yandex.ru>
2025-01-02 15:53:23 +0300

    fix names

--- tests/test_bun.py
+++ tests/test_bun.py
@@ -19,10 +19,10 @@ class TestBun:
 
     def test_bun_string_name(self):
         """Проверяем, что имя булочки корректно сохраняется как строка."""
-        bun = Bun("Круглая>", 3.0)
+        bun = Bun("Круглая булка", 3.0)
         assert isinstance(bun.get_name(), str)
 
     def test_bun_float_price(self):
         """Проверяем, что цена булочки корректно сохраняется как float."""
-        bun = Bun("Квадратик", 1.75)
+        bun = Bun("Квадратная булка", 1.75)
         assert isinstance(bun.get_price(), float)
